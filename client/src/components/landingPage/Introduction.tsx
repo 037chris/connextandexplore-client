732964f7889fa9b8b6a6b8b46788850d4ae3335a
Maryam Daliri <s51677@beuth-hochschule.de>
2023-11-24 14:37:27 +0100

    startSeite

---
+++ client/src/components/landingPage/Introduction.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { CgCopy } from "react-icons/cg";
+
+interface IntroProps {
+
+    
+}
+
+const LocalEvents: React.FC<IntroProps> = ({
+
+}) => {
+  
+    return ( 
+        <div className="bg-white flex w-full p-4">
+            <div className="grid grid-cols-3 place-items-center w-full">
+                <div className="col-span-2">
+                    <CgCopy className="w-90 text-blue-600 text-center" />
+                    <p className="text-center font-serif text-lg text-gray-800 mb-4">Mach die Stadt zu deinem Abenteuer</p>
+                    <p className="text-center font-sans text-sm mb-4">Willkommen bei Click & Connect! 
+                    Tauche ein in unsere Eventplattform und entdecke spannende Abenteuer in deiner Stadt.
+                     Erlebe die Vielfalt deiner Stadt auf völlig neue Weisen und knüpfe wertvolle Verbindungen. Sei Teil unserer Community und gestalte deine Freizeit auf einzigartige Art und Weise.</p>
+                  
+                    <br />
+                    <form action="" className="bg-blue-500 rounded ">
+                        <input type="text" placeholder="Event suchen" className="shadow-inner"/>
+                        <input type="number" placeholder="Postleitzahl" className="shadow-inner"/>
+                        <input type="button" value="Suchen" className="px-3 text-white"/>
+                    </form>
+                    
+
+                </div>
+                <div className="col-span-1">
+                    <img className="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdzhqM1WMJRsY9lYJ0Gi5OknZJiahj52a76iuVwB69fA&s" alt="" />
+                </div>
+            </div>
+        </div>
+  );
+};
+
+ 
+export default LocalEvents;
