6e2f28548a7dfbffacb8d1aff8014a48a1c72172
Chris <chrsd@gmx.de>
2023-11-25 16:33:12 +0100

    Personal Info Settings

--- client/src/components/pages/UserSettings.tsx
+++ client/src/components/pages/UserSettings.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import Heading from "../Heading";
 import { SettingsNav } from "../settingsNavBar/SettingsNav";
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
+import Autocomplete from "react-google-autocomplete";
 
 interface UserSettingsProps {
     activeTab: 'profile' | 'info' | 'account';
 }
 
 const UserSettings: React.FC<UserSettingsProps> = ({ activeTab }) => {
+    // calender
+    const [selected, setSelected] = useState<Date>();
+    // selected date calender
+    let footer = <p>Wann hast du Geburtstag?</p>
+    if (selected) {
+        footer = <p>{format(selected, 'PP')}</p>
+    }
 
     return (
         <>
@@ -19,27 +29,62 @@ const UserSettings: React.FC<UserSettingsProps> = ({ activeTab }) => {
                     <SettingsNav />
                 </div>
                 <div className="cols-span-2">
-                    {/* {activeTab}
-                    {activeTab === 'profile' && <div>Profil Seite hier rendern</div>}
-                    {activeTab === 'info' && <div>Info Seite hier rendern</div>}
-                    {activeTab === 'account' && <div>Account Seite hier rendern</div>} */}
+                    <div className="grid">
+                        {/* {activeTab}
+                        {activeTab === 'profile' && <div>Profil Seite hier rendern</div>}
+                        {activeTab === 'info' && <div>Info Seite hier rendern</div>}
+                        {activeTab === 'account' && <div>Account Seite hier rendern</div>} */}
 
-                    <div>
+                        <div>
+                            <p>Profile Settings</p>
+                            <form>
+                                {/* hier fehlt das ausgelesene Profilbild */}
+                                <input type="submit" value="Neuer upload" />
+                                <input type="submit" value="Foto löschen" />
+                            </form>
+                            <form>
+                                <label htmlFor="vorname">Vorname</label>
+                                <input id="vorname" className="border-slim" type="text" placeholder="Vorname"></input>
+                                <label htmlFor="name">Name</label>
+                                <input id="name" className="border-slim" type="text" placeholder="Name"></input>
+                                <span>Social Media Links</span>
+                                <label htmlFor="instagram">Instagram</label>
+                                <input id="instagram" className="border-slim" type="text" placeholder="Instagram Profil URL"></input>
+                                <label htmlFor="facebook">Facebook</label>
+                                <input id="facebook" className="border-slim" type="text" placeholder="Facbook Profil URL"></input>
+                                <input type="submit" value="speichern" />
+                            </form>
+                        </div>
+                    </div>
+                    <div className="grid pt-6">
+                        <p>Personal Info Settings</p>
                         <form>
-                            {/* hier fehlt das ausgelesene Profilbild */}
-                            <input type="submit" value="Neuer upload" />
-                            <input type="submit" value="Foto löschen" />
-                        </form>
-                        <form>
-                            <label id="vorname"></label>
-                            <input id="vorname" className="border-slim" type="text" placeholder="Vorname"></input>
-                            <label id="name"></label>
-                            <input id="name" className="border-slim" type="text" placeholder="Name"></input>
-                            <span>Social Media Links</span>
-                            <label id="instagram"></label>
-                            <input id="instagram" className="border-slim" type="text" placeholder="Instagram Profil URL"></input>
-                            <label id="facebook"></label>
-                            <input id="facebook" className="border-slim" type="text" placeholder="Facbook Profil URL"></input>
+                            {/* noch in der bearbeitung */}
+                            {/* <DayPicker 
+                                mode='single'
+                                ISOWeek
+                                defaultMonth={new Date(1979, 8)} // falls datum bereits eingegeben
+                                selected={selected}
+                                onSelect={setSelected}
+                                footer={footer}
+                            /> */}
+                            <label htmlFor="gender">Geschlecht</label>
+                            <select name="gender" id="gender">
+                                <option value="male">männlich</option>
+                                <option value="female">weiblich</option>
+                                <option value="diverse">divers</option>
+                                <option value="noinfo">ohne Angabe</option>
+                            </select>
+                            {/* https://www.npmjs.com/package/react-google-autocomplete?activeTab=readme */}
+                            <Autocomplete
+                                apiKey={process.env.REACT_APP_GOOGLE}
+                                defaultValue={'Berlin'} //Adresse aus User
+                                onPlaceSelected={(place) => {
+                                    console.log(place);
+                                }}
+                            />
+
+
                             <input type="submit" value="speichern" />
                         </form>
                     </div>
