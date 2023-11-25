164c883348ee04359c6d8e3039c6d91f311af9dc
Chris <chrsd@gmx.de>
2023-11-25 17:48:56 +0100

    Setting forms 1.0

--- client/src/components/pages/UserSettings.tsx
+++ client/src/components/pages/UserSettings.tsx
@@ -88,6 +88,37 @@ const UserSettings: React.FC<UserSettingsProps> = ({ activeTab }) => {
                             <input type="submit" value="speichern" />
                         </form>
                     </div>
+                    <div className="grid pt-6">
+                        <p>Account Setting</p>
+                        <form>
+                            <label htmlFor="email">Vorname</label>
+                            <input id="email" className="border-slim" type="text" placeholder="E-Mail"></input>
+                            {/* Passwortabfrage beim speichern */}
+                            <label htmlFor="language">Sprache</label>
+                            <select name="language" id="language">
+                                <option value="german">deutsch</option>
+                                <option value="english">english</option>    
+                            </select>
+                            <input type="submit" value="speichern" />
+                        </form>
+
+                        <div>
+                            <p>Passwort ändern</p>
+                            <p>Wenn du dein Passwort änderst, wirst du automatisch ausgeloggt.</p>
+                            <button className="settings-btn">Passwort ändern</button>
+                        </div>
+                        <div>
+                            <p>Account deaktivieren</p>
+                            <p>Du kannst deinen Account vorübergehend deaktivieren. Meld dich einfach erneut an für eine Reaktivierung. </p>
+                            <button className="settings-btn">Account deaktivieren</button>
+                        </div>
+                        <hr />
+                        <div>
+                            <p>Account löschen</p>
+                            <p>Wenn du Connect and Explore erneut verwenden möchtest, musst du einen neuen Account erstellen.</p>
+                            <button className="settings-btn">Account löschen</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
