1daaf3b8bf889f8c1d20a16482c49264c8512d3a
s88268 <s88268@bht-berlin.de>
2023-11-25 17:53:13 +0100

    error handling api

--- client/src/Resources.ts
+++ client/src/Resources.ts
@@ -25,6 +25,41 @@ export type  UserRegistration = {
   };
 }
 
+export type userResource = {
+  id?: string;
+  email: string;
+  name: {
+    first: string;
+    last: string;
+  };
+  password?: string;
+  isAdministrator: Boolean;
+  address: addressResource;
+  profilePicture?: string;
+  birthDate: Date;
+  gender: string;
+  socialMediaUrls?: {
+    facebook?: string;
+    instagram?: string;
+  };
+  isActive: boolean;
+  oldPassword?: string;
+};
+
+export type usersResource = {
+  users: userResource[];
+};
+
+export type addressResource = {
+  id?: string;
+  street: String;
+  houseNumber: String;
+  apartmentNumber?: String;
+  postalCode: String;
+  city: String;
+  stateOrRegion?: String;
+  country: String;
+};
 
 export type ValidationMessages<Type> = {
   [Property in keyof Type]?: string;
