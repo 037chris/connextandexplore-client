f82251edd80c48da464c26e7dc8950d4febeb3fa
Chris <chrsd@gmx.de>
2023-11-26 13:44:54 +0100

    georg

--- client/src/components/pages/UserSettings.tsx
+++ client/src/components/pages/UserSettings.tsx
@@ -21,13 +21,44 @@ const UserSettings: React.FC<SettingsNavProps> = ({ }) => {
 
     return (
         <>
-            <div className="max-grid">
-                <div className='p-3 max-w-lg mx-auto '>
-                    <h1 className='text-3xl text-center font-semibold mt-20 '>Settings</h1>
-                    <Heading title='Profil bearbeiten' subtitle="Diese Informationen erscheinen in deinem öffentlichen Profil" />
+            <div>
+                <div className='p-3'>
+                    <h1 className='text-center font-semibold mt-20'>Settings</h1>
                 </div>
-                <div className='grid lg:grid-cols-3 gap-6'>
-                    <div className="cols-span-1">
+                <div className="max-grid">
+                    <div className='grid grid-cols-1 grid-rows-2 md:grid-cols-3 md:grid-rows-1'>
+                        <div className="col-span-1 bg-blue-200">
+                            <div className='border-box'>
+                                <ul>
+                                    <li>
+                                        <a href="#" className={activeTab === 'profile' ? 'active' : ''} onClick={() => handleTabClick('profile')}>
+                                            C&E Profil
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className={activeTab === 'info' ? 'active' : ''} onClick={() => handleTabClick('info')}>
+                                            Personal Info
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className={activeTab === 'account' ? 'active' : ''} onClick={() => handleTabClick('account')}>
+                                            Account
+                                        </a>
+                                    </li>
+                                </ul>
+                                <a href="#" className="help-link">Help</a>
+                            </div>
+                        </div>
+                        <div className="col-span-1 md:col-span-2 bg-red-100 min-h-screen">
+
+
+                        </div>
+                    </div>
+                </div>
+
+                {/*
+                <div className='grid grid-cols-1 grid-rows-2 lg:grid-cols-3 lg:grid-rows-1'>
+                    <div className="col-span-1">
                         <div className='border-box'>
                             <Container>
                                 <ul>
@@ -51,14 +82,14 @@ const UserSettings: React.FC<SettingsNavProps> = ({ }) => {
                             </Container>
                         </div>
                     </div>
-                    <div className="cols-span-2">
-                        <div className="grid">
+                    <div className="col-span-1 lg:col-span-2">
+                        <div className="">
                             {activeTab === 'profile' && <ProfileSettingsComponent />}
                             {activeTab === 'info' && <PersonalInfoSettingsComponent />}
                             {activeTab === 'account' && <AccountSettingsComponent />}
                         </div>
                     </div>
-                </div>
+                </div>*/}
 
             </div>
             <Footer />
--- client/src/components/web/Footer.tsx
+++ client/src/components/web/Footer.tsx
@@ -3,13 +3,13 @@ const Footer = () => {
         <footer>
             <div className="max-grid">
                 <div className="grid grid-cols-12">
-                    <div className="col-span-4">
+                    <div className="col-span-12 md:col-span-5 lg:col-span-4">
                         <p>4/12 Breite</p>
                     </div>
-                    <div className="col-span-1">
+                    <div className="col-span-12 md:col-span-2 lg:col-span-1">
                         <p>1/12 Breite</p>
                     </div>
-                    <div className="col-span-7">
+                    <div className="col-span-12 md:col-span-5 lg:col-span-7">
                         <p>7/12 Breite</p>
                     </div>
                 </div>
