cecac4a1bec707e24c92d81f6d86cec72583684e
khatiazitanishvili <s86156@bht-hochschule.de>
2024-01-20 21:55:56 +0100

    add submit function

--- client/src/components/allEvents/AllEvents.tsx
+++ client/src/components/allEvents/AllEvents.tsx
@@ -10,7 +10,7 @@ import React from "react";
 import { useForm, FieldValues } from "react-hook-form";
 import Input from "../html/inputs/Input";
 import Button from "../html/Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface AllEventsProps {
   //events: { date: any, name: string, description: string, imageUrl?: string, hashtags?:string[],category?: string }[]
@@ -34,29 +34,25 @@ const initEvent: eventResource = {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp4zpIuafwUwAALiZhyewnZPBfWlm8OvxZIEawUIuHKw&s",
 };
 
-const AllEvents: React.FC<AllEventsProps> = ({}) => {
+
+const AllEvents: React.FC<AllEventsProps> = () => {
   const [dbEvents, setDbEvents] = useState({ events: [initEvent] });
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
+  const location = useLocation(); // Use useLocation hook to get location state
+
   const load = async () => {
-    //let allDbEvents=await getAllEvents();
-    //if(allDbEvents===-1)return
     setDbEvents(await getAllEvents());
   };
 
   useEffect(() => {
-    // Hier wird der Filter angewendet, basierend auf der ausgewählten Kategorie
     const filteredEvents = dbEvents.events.filter(
       (event) =>
         event.category &&
         event.category.some((category) => category.name === selectedCategory)
     );
-    // console.log(filteredEvents)
-    // console.log(selectedCategory)
     load();
     document.title = "Alle Events bei uns - Connect & Explore";
-
-    // Setze den Zustand mit den gefilterten Events
     setDbEvents({ events: filteredEvents });
   }, [selectedCategory]);
 
@@ -68,17 +64,13 @@ const AllEvents: React.FC<AllEventsProps> = ({}) => {
   const [plz, setPLZ] = React.useState<string>("");
   const [loading, setLoading] = React.useState(false);
 
-  const {
-    register,
-    //setError,
-    formState: { errors },
-  } = useForm<FieldValues>({});
-
+  const { register, setValue, formState: { errors } } = useForm<FieldValues>({});
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement> | File) => {
     if (e instanceof File || e === null) {
       return;
     }
+
     const { name, value } = e.target;
     if (name === "query") {
       setQuery(value);
@@ -87,6 +79,20 @@ const AllEvents: React.FC<AllEventsProps> = ({}) => {
     }
   };
 
+  useEffect(() => {
+    // Set default values from location state
+    const { state } = location;
+    if (state && state.defaultValues) {
+      setQuery(state.defaultValues.event || "");
+      setPLZ(state.defaultValues.place || "");
+    }
+  }, [location, location.state]);
+
+  useEffect(() => {
+    // Set default values as default values for the Input components
+    setValue("query", query);
+    setValue("plz", plz);
+  }, [query, plz, setValue]);
   return (
     <>
       <Header homeRoute={"page"} headline={"Alle Events"} />
